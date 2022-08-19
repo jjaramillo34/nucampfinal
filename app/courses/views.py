ca9d24f6316ad30bbf6a1ba07a0d1a30d6203ecb
Javier Jaramillo <jjaramillo34@gmail.com>
2022-08-19 16:36:03 -0400

    Update courses views

--- app/courses/views.py
+++ app/courses/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from courses.models import Course
-
+from home.models import Home
 
 # Create your views here.
 
 def courses(request):
+    homepage = Home.objects.filter(status=1)
     courses = Course.objects.filter(status=1).order_by('-obtained_date')
-    context = {'courses': courses}
+    context = {
+        'homepage': homepage,
+        'courses': courses
+        }
     print(context)
     return render(
         request=request,
