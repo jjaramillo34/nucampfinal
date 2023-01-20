537e3a56817ae7c8149a4eb5ce121e5e7464007f
Javier Jaramillo <jjaramillo34@gmail.com>
2023-01-19 21:12:05 -0500

    Update files

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -18,17 +18,17 @@ from pathlib import Path
 from decouple import config
 from django.utils.translation import gettext_lazy as _
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://a13712edbde34d259270b3c394b4a996@o1369764.ingest.sentry.io/6672943",
-    integrations=[
-        DjangoIntegration(
-            transaction_style='url',
-        ),
-    ],
-)
+#import sentry_sdk
+#from sentry_sdk.integrations.django import DjangoIntegration
+#
+#sentry_sdk.init(
+#    dsn="https://a13712edbde34d259270b3c394b4a996@o1369764.ingest.sentry.io/6672943",
+#    integrations=[
+#        DjangoIntegration(
+#            transaction_style='url',
+#        ),
+#    ],
+#)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = config('DEBUG')
@@ -281,7 +281,7 @@ STATICFILES_FINDERS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/en/'
 LOGIN_URL = '/user/login/'
 
 # social auth configs for github
