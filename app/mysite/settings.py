917592f0058ae55ccf87fa0492041b688bd74579
Javier Jaramillo <jjaramillo34@gmail.com>
2023-01-21 16:38:19 -0500

    Update Settings file

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -344,9 +344,9 @@ CKEDITOR_CONFIGS = {
 #CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Heroku configs
-django_heroku.settings(locals())
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#django_heroku.settings(locals())  
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
